6ec4ac2f888f7a06f3d31c933fe558c5472b1ad6
flexilef <iflex606@gmail.com>
2015-11-23 19:23:47 -0800

    Added contact page form Added projects page thumbnails

--- index.php
+++ index.php
@@ -6,66 +6,66 @@ and open the template in the editor.
 -->
 
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Home | Felix Lee</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <link href="boottest.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <!-- Wrap all page content here -->
-        <div id="wrap">
+    <title>Home | Felix Lee</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="boottest.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <!-- Wrap all page content here -->
+    <div id="wrap">
 
-            <!-- Fixed navbar -->
-            <div class="navbar navbar-inverse navbar-static-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="index.php">PROJECT NAME</a>
-                    </div>
-                    <div class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav">
-                            <li><a href="blog.html">Blog</a></li>
-                            <li><a href="projects.html">Projects</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                    </div><!--/.nav-collapse -->
-                </div>
-            </div>
-
-            <!-- Begin page content -->
-            <div class="container">
-                <div class="page-header">
-                    <h1>Under construction...</h1>
-                </div>
-                <div class="page-content">
-                    <p>...</p>
-                </div>
-            </div>
+      <!-- Fixed navbar -->
+      <div class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">PROJECT NAME</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="projects.html">Projects</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
         </div>
+      </div>
 
-        <div id="footer">
-            <div class="container">
-                <ul>
-                    <li><a class="facebook expand" href="#"></a></li>
-                    <li><a class="twitter expand" href="#"></a></li>                    
-                    <li><a class="github expand" href="#"></a></li>
-                </ul>
-                <p class="text-muted">&copy Felix Lee 2015</p>
-            </div>
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Under construction...</h1>
         </div>
+        <div class="page-content">
+          <p>...</p>
+        </div>
+      </div>
+    </div>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-    </body>
+    <div id="footer">
+      <div class="container">
+        <ul>
+          <li><a class="facebook expand" href="#"></a></li>
+          <li><a class="twitter expand" href="#"></a></li>                    
+          <li><a class="github expand" href="#"></a></li>
+        </ul>
+        <p class="text-muted">&copy Felix Lee 2015</p>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
